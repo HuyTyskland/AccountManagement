61cfe2c1ff59dff90f8289f5a3ad22121fe5576e
Huy3.nguyen <huy3.nguyen@lge.com>
2021-01-19 12:14:01 +0700

    create User class

---
+++ User.cpp
@@ -0,0 +1,19 @@
+/*
+ * User.cpp
+ *
+ *  Created on: Jan 19, 2021
+ *      Author: huy3.nguyen
+ */
+
+#include<iostream>
+#include "Account.h"
+#include "Admin.h"
+#include "User.h"
+
+using namespace std;
+
+User::User() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ User.h
@@ -0,0 +1,16 @@
+/*
+ * User.h
+ *
+ *  Created on: Jan 19, 2021
+ *      Author: huy3.nguyen
+ */
+
+#ifndef USER_H_
+#define USER_H_
+
+class User {
+public:
+	User();
+};
+
+#endif /* USER_H_ */
