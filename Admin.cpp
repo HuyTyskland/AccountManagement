2a4640e42457c3812895f8635d6c805c47b3f5be
huy3.nguyen <huy3.nguyen@lge.com>
2021-01-19 10:45:21 +0700

    create Admin class

---
+++ Admin.cpp
@@ -0,0 +1,14 @@
+/*
+ * Admin.cpp
+ *
+ *  Created on: Jan 19, 2021
+ *      Author: huy3.nguyen
+ */
+
+#include "Admin.h"
+
+Admin::Admin() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ Admin.h
@@ -0,0 +1,16 @@
+/*
+ * Admin.h
+ *
+ *  Created on: Jan 19, 2021
+ *      Author: huy3.nguyen
+ */
+
+#ifndef ADMIN_H_
+#define ADMIN_H_
+
+class Admin {
+public:
+	Admin();
+};
+
+#endif /* ADMIN_H_ */
